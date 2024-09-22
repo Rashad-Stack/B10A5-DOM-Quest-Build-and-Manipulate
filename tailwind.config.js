6b457a0317ec04361552ed6dedc5b1b57df0d50e
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-09-22 20:21:24 +0600

    Add Nav and blogs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       colors: {
         primary: "rgb(180, 244, 97)",
+        secondary: "rgb(249, 247, 243)",
+        "dark-primary": "rgb(17, 17, 17)",
+        "dark-secondary": "rgba(17, 17, 17, 0.7)",
       },
       fontFamily: {
         lexend: "Lexend", // Adds a new `font-display` class
